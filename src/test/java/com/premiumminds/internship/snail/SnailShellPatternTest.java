726f97addeade8ca7a1b71e1d8afbc2e40dc2279
Andureh <135390161+Andureh@users.noreply.github.com>
2023-06-05 18:08:40 +0100

    Update SnailShellPatternTest.java

--- src/test/java/com/premiumminds/internship/snail/SnailShellPatternTest.java
+++ src/test/java/com/premiumminds/internship/snail/SnailShellPatternTest.java
@@ -33,5 +33,36 @@ public class SnailShellPatternTest {
     int[] result = count.get(10, TimeUnit.SECONDS);
     int[] expected = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
     assertArrayEquals(result, expected);
+    
+    int[][] matrix = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
+    Future<int[]> count = new SnailShellPattern().getSnailShell(matrix);
+    int[] result = count.get(10, TimeUnit.SECONDS);
+    int[] expected = { 1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10 };
+    assertArrayEquals(result, expected);
+    
+    int[][] matrix = {
+            { 1,  2,  3,  4,  5 },
+            { 6,  7,  8,  9,  10 },
+            { 11, 12, 13, 14, 15 },
+            { 16, 17, 18, 19, 20 },
+            { 21, 22, 23, 24, 25 }
+    };
+    Future<int[]> count = new SnailShellPattern().getSnailShell(matrix);
+    int[] result = count.get(10, TimeUnit.SECONDS);
+    int[] expected = { 1, 2, 3, 4, 5, 10, 15, 20, 25, 24, 23, 22, 21, 16, 11, 6, 7, 8, 9, 14, 19, 18, 17, 12, 13 };
+    assertArrayEquals(result, expected);
+    
+        int[][] matrix = {
+            { 1,  2,  3,  4,  5,  6 },
+            { 7,  8,  9,  10, 11, 12 },
+            { 13, 14, 15, 16, 17, 18 },
+            { 19, 20, 21, 22, 23, 24 },
+            { 25, 26, 27, 28, 29, 30 },
+            { 31, 32, 33, 34, 35, 36 }
+    };
+    Future<int[]> count = new SnailShellPattern().getSnailShell(matrix);
+    int[] result = count.get(10, TimeUnit.SECONDS);
+    int[] expected = { 1, 2, 3, 4, 5, 6, 12, 18, 24, 30, 36, 35, 34, 33, 32, 31, 25, 19, 13, 7, 8, 9, 10, 11, 17, 23, 29, 28, 27, 26, 20, 14, 15, 16, 22, 21 };
+    assertArrayEquals(result, expected);
   }
 }
